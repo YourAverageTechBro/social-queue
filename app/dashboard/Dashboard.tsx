ad3f4aea08f1a05052c31a020f9f82caca15c3b7
Dohyun Kim <dohyun@youraveragetechbro.com>
2024-06-06 11:55:28 -0500

    Store instagram account in db

--- app/dashboard/Dashboard.tsx
+++ app/dashboard/Dashboard.tsx
@@ -1,14 +1,31 @@
 "use client";
 
 import ConnectFacebookAccountButton from "@/app/dashboard/ConnectFacebookAccountButton";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { InstagramAccount } from "@/utils/facebookSdk";
+import { saveInstagramAccount } from "@/app/actions/instagramAccounts";
+import { useFormState } from "react-dom";
+import { Button } from "@/components/common/Button";
+import toast from "react-hot-toast";
 
-export default function Dashboard() {
+export default function Dashboard({ userId }: { userId: string }) {
   const [appScopedUserId, setAppScopedUserId] = useState<string>("");
   const [instagramAccounts, setInstagramAccounts] = useState<
     InstagramAccount[]
   >([]);
+  const [state, formAction] = useFormState(saveInstagramAccount, {
+    error: null,
+    data: "",
+  });
+
+  useEffect(() => {
+    if (state.error) {
+      toast.error(state.error);
+    } else if (state.data) {
+      toast.success(state.data);
+    }
+  }, [state]);
+
   return (
     <div>
       <ConnectFacebookAccountButton
@@ -16,9 +33,41 @@ export default function Dashboard() {
         setInstagramAccounts={setInstagramAccounts}
       />
       {instagramAccounts.map((account) => (
-        <div className={"border-2 "} key={account.id}>
-          {account.name}
-        </div>
+        <form
+          action={formAction}
+          className={"border-2 border-white p-4 rounded-lg"}
+          key={account.id}
+        >
+          <input
+            type={"hidden"}
+            name={"appScopedUserId"}
+            value={appScopedUserId}
+          />
+          <input
+            type={"hidden"}
+            name={"shortLivedAccessToken"}
+            value={account.access_token}
+          />
+          <input
+            type={"hidden"}
+            name={"instagramBusinessAccountId"}
+            value={account.instagram_business_account.id}
+          />
+          <input type={"hidden"} name={"facebookPageId"} value={account.id} />
+          <input
+            type={"hidden"}
+            name={"instagramAccountName"}
+            value={account.name}
+          />
+          <input
+            type={"hidden"}
+            name={"pictureUrl"}
+            value={account.picture.data.url}
+          />
+          <input type={"hidden"} name={"userId"} value={userId} />
+          <p>{account.name} </p>
+          <Button type={"submit"}>Save Account</Button>
+        </form>
       ))}
     </div>
   );
--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,8 @@
-import { createBrowserClient } from "@supabase/ssr";
+import {createBrowserClient} from "@supabase/ssr";
+import {Database} from "@/types/supabase";
 
 export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+    createBrowserClient<Database>(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    );
