2b4e09f3437e4845d77c668219fad2f71bb36d45
Dohyun Kim <dohyun@youraveragetechbro.com>
2024-06-11 18:48:16 -0500

    Add ability to delete social account

---
+++ components/common/Modal.tsx
@@ -0,0 +1,60 @@
+import { Dispatch, Fragment, ReactNode, SetStateAction } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+
+export default function Modal({
+  children,
+  open,
+  setOpen,
+  onCloseCallback,
+  canClose = true,
+}: {
+  children: ReactNode;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  onCloseCallback?: () => void;
+  canClose?: boolean;
+}) {
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        className="relative z-20"
+        onClose={(value) => {
+          if (canClose) {
+            setOpen(value);
+            onCloseCallback?.();
+          }
+        }}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform rounded-lg bg-gray-800 px-8 py-10 text-left shadow-xl transition-all w-[90%]">
+                {children}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
